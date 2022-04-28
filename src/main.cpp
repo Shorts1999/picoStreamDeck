5e640c011af680e925fa3de5db13237d515e938b
Shorts1999 <sjorssmit1999@gmail.com>
2022-04-28 15:04:47 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+
+#include "USBKeyboard.h"
+
+const uint8_t pushbuttons[4] = { p2, p3, p4, p5 };
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  for (uint8_t i = 0; i < sizeof(pushbuttons); i++) {
+    pinMode(pushbuttons[i], INPUT_PULLUP);
+  }
+}
+
+void loop() {
+  for(uint8_t i =0; i<sizeof(pushbuttons); i++){
+    if(!digitalRead(pushbuttons[i])){
+      switch(i){
+        case 0:
+          
+          break;
+        case 1: 
+          
+          break;
+        case 2: 
+          
+          break;
+        case 3:
+          
+      }
+    delay(500);
+    }
+  }
+}
---
+++ upload.bat
@@ -0,0 +1,2 @@
+pio run
+copy ".pio\build\pico\firmware.uf2" "D:\"
